ca4618830453921be9a2a7db52e3aa1b9a53a783
Irene de la Torre <irene.delatorre@gmail.com>
2022-05-08 16:53:28 +0200

    Changed english embeds

--- en/app.js
+++ en/app.js
@@ -1,8 +1,8 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/@irenedelatorre/participacion-espanola-en-eurovision-un-enfoque-de-genero.js?v=3";
+import define from "https://api.observablehq.com/@irenedelatorre/spanish-participation-in-eurovision-a-gender-approach-to-c.js?v=3";
 
 new Runtime().module(define, name => {
-  if (name === "style") return new Inspector(document.querySelector("#observablehq-css"));
+  if (name === "css") return new Inspector(document.querySelector("#observablehq-css"));
   if (name === "chart_participation") return new Inspector(document.querySelector("#observablehq-chart_participation"));
   if (name === "viewof radios_type") return new Inspector(document.querySelector("#observablehq-viewof-radios_type"));
   if (name === "viewof align_b_comp") return new Inspector(document.querySelector("#observablehq-viewof-align_b_comp"));
@@ -13,10 +13,10 @@ new Runtime().module(define, name => {
     "composers_chart",
     "Tooltip",
     "composers_tooltip",
-    "Composers_es",
+    "Composers_en",
     "singers_tooltip",
     "singers_chart",
-    "Singers_es"
+    "Singers_en"
     ]
     .includes(name);
 });
