6c71c0b8ffbf16bc79614616ef2b748ed9ef450e
Irene de la Torre <irene.delatorre@gmail.com>
2022-05-08 16:45:39 +0200

    Added english version, fixed typos

---
+++ en/app.js
@@ -0,0 +1,22 @@
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import define from "https://api.observablehq.com/@irenedelatorre/participacion-espanola-en-eurovision-un-enfoque-de-genero.js?v=3";
+
+new Runtime().module(define, name => {
+  if (name === "style") return new Inspector(document.querySelector("#observablehq-css"));
+  if (name === "chart_participation") return new Inspector(document.querySelector("#observablehq-chart_participation"));
+  if (name === "viewof radios_type") return new Inspector(document.querySelector("#observablehq-viewof-radios_type"));
+  if (name === "viewof align_b_comp") return new Inspector(document.querySelector("#observablehq-viewof-align_b_comp"));
+  if (name === "chart_composers") return new Inspector(document.querySelector("#observablehq-chart_composers"));
+  if (name === "resize") return new Inspector(document.querySelector("#observablehq-resize"));
+
+  return [
+    "composers_chart",
+    "Tooltip",
+    "composers_tooltip",
+    "Composers_es",
+    "singers_tooltip",
+    "singers_chart",
+    "Singers_es"
+    ]
+    .includes(name);
+});
